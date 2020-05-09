b2425eea665c7b2f356f5490bd3c465578c291f0
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2020-05-09 16:05:28 +0545

    static changes

--- imdb/settings.py
+++ imdb/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='static/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static"),
